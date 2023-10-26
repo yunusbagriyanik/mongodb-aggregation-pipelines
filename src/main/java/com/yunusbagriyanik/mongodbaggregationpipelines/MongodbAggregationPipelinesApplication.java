23b100816d64009a8c29ed7143fe87cf286fdb99
Yunus Bagriyanik <yunusbgrynk@gmail.com>
2023-10-26 21:33:41 +0300

    initial commit

---
+++ src/main/java/com/yunusbagriyanik/mongodbaggregationpipelines/MongodbAggregationPipelinesApplication.java
@@ -0,0 +1,13 @@
+package com.yunusbagriyanik.mongodbaggregationpipelines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbAggregationPipelinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbAggregationPipelinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunusbagriyanik/mongodbaggregationpipelines/MongodbAggregationPipelinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunusbagriyanik.mongodbaggregationpipelines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbAggregationPipelinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
